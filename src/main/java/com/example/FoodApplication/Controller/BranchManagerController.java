2e8fda8c04a6659bf52deb5fac3f30289272fd5e
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 19:35:13 +0530

    Update BranchManagerController.java

--- src/main/java/com/example/FoodApplication/Controller/BranchManagerController.java
+++ src/main/java/com/example/FoodApplication/Controller/BranchManagerController.java
@@ -11,9 +11,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.FoodApplication.Service.BranchManagerService;
@@ -25,6 +26,7 @@ import com.example.FoodApplication.util.ResponseStructure;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
+
 public class BranchManagerController {
 
 	@Autowired
@@ -39,8 +41,8 @@ public class BranchManagerController {
 		return branchManagerService.saveBranchManager(branchManager);
 	}
 
-	@DeleteMapping("/deletebm")
-	public ResponseEntity<ResponseStructure<BranchManager>> deleteBranchManager(@RequestParam int id) {
+	@DeleteMapping("/deletebm/{id}")
+	public ResponseEntity<ResponseStructure<BranchManager>> deleteBranchManager(@PathVariable int id) {
 		return branchManagerService.deleteBranchManager(id);
 	}
 
@@ -52,6 +54,11 @@ public class BranchManagerController {
 	@GetMapping("/allbm")
 	public ResponseEntity<ResponseStructure<List<BranchManager>>>  findAllBranchManagers() {
 		return branchManagerService.findAllBranchManagers();
+	}
+	
+	@PutMapping("/updatebm/{id}")
+	public ResponseEntity<ResponseStructure<BranchManager>> updateBranchManager(@RequestBody BranchManager branchManager, @PathVariable int id) {
+		return branchManagerService.updateBranchManager(branchManager, id);
 	}
 
 }
